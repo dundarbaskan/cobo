1c56d1962a02e9474dabdcf94fbef09ff79193a4
a-social <sahandamar274@gmail.com>
2026-02-04 17:21:13 +0200

    added pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    apps: [
+        {
+            name: "COBO-API",
+            script: "main.py",
+            interpreter: "python", // veya venv yolu: "venv/Scripts/python.exe"
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                PORT: 8000,
+                NODE_ENV: "production",
+            },
+            // Log Ayarları
+            error_file: "./logs/api-error.log",
+            out_file: "./logs/api-out.log",
+            time: true // Loglara tarih ekler
+        },
+        {
+            name: "COBO-MT5-WORKER",
+            script: "mt5_worker.py",
+            interpreter: "python",
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            // Log Ayarları
+            error_file: "./logs/mt5-error.log",
+            out_file: "./logs/mt5-out.log",
+            time: true
+        },
+
+    ]
+};
