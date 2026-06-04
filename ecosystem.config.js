5b5b7037150d0058dff9a54d4ef25e21b8590cfd
dundarbaskan <erdalamir12@gmail.com>
2026-06-04 20:32:30 +0300

    fixed exosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,57 +1,59 @@
 module.exports = {
-    apps: [
-        {
-            name: "COBO-API",
-            script: "main.py",
-            interpreter: "./sanallik/Scripts/python.exe",
-            interpreter_args: "-X utf8",
-            instances: 1,
-            autorestart: true,
-            watch: false,
-            max_memory_restart: '1G',
-            // Varsayılan "pm2 start ecosystem.config.js" -> Canlı (Release)
-            env: {
-                PORT: 8000,
-                ENVIRONMENT: "release",
-                NODE_ENV: "production",
-                PYTHONIOENCODING: "utf-8",
-                PYTHONUTF8: "1"
-            },
-
-                ENVIRONMENT: "test",
-                NODE_ENV: "development",
-                PYTHONIOENCODING: "utf-8",
-                PYTHONUTF8: "1"
-            },
-            // Log Ayarları
-            error_file: "./logs/api-error.log",
-            out_file: "./logs/api-out.log",
-            time: true // Loglara tarih ekler
-        },
-        {
-            name: "COBO-MT5",
-            script: "mt5_worker.py",
-            interpreter: "./sanallik/Scripts/python.exe",
-            interpreter_args: "-X utf8",
-            instances: 1,
-            autorestart: true,
-            watch: false,
-            // Varsayılan
-            env: {
-                ENVIRONMENT: "release",
-                PYTHONIOENCODING: "utf-8",
-                PYTHONUTF8: "1"
-            },
-            // Test
-            env_test: {
-                ENVIRONMENT: "test",
-                PYTHONIOENCODING: "utf-8",
-                PYTHONUTF8: "1"
-            },
-            // Log Ayarları
-            error_file: "./logs/mt5-error.log",
-            out_file: "./logs/mt5-out.log",
-            time: true
-        }
-    ]
+  apps: [
+    {
+      name: "COBO-API",
+      script: "main.py",
+      interpreter: "./sanallik/Scripts/python.exe",
+      interpreter_args: "-X utf8",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      // Varsayılan (pm2 start ecosystem.config.js)
+      env: {
+        PORT: 8000,
+        ENVIRONMENT: "release",
+        NODE_ENV: "production",
+        PYTHONIOENCODING: "utf-8",
+        PYTHONUTF8: "1",
+      },
+      // Test Modu (pm2 start ecosystem.config.js --env test)
+      env_test: {
+        PORT: 8000,
+        ENVIRONMENT: "test",
+        NODE_ENV: "development",
+        PYTHONIOENCODING: "utf-8",
+        PYTHONUTF8: "1",
+      },
+      // Log Ayarları
+      error_file: "./logs/api-error.log",
+      out_file: "./logs/api-out.log",
+      time: true,
+    },
+    {
+      name: "COBO-MT5",
+      script: "mt5_worker.py",
+      interpreter: "./sanallik/Scripts/python.exe",
+      interpreter_args: "-X utf8",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      // Varsayılan
+      env: {
+        ENVIRONMENT: "release",
+        PYTHONIOENCODING: "utf-8",
+        PYTHONUTF8: "1",
+      },
+      // Test
+      env_test: {
+        ENVIRONMENT: "test",
+        PYTHONIOENCODING: "utf-8",
+        PYTHONUTF8: "1",
+      },
+      // Log Ayarları
+      error_file: "./logs/mt5-error.log",
+      out_file: "./logs/mt5-out.log",
+      time: true,
+    },
+  ],
 };
